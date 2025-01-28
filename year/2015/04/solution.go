bb16d46e94b7e02c1e377edb927c4029390445b8
gkotecki <gkotecki@users.noreply.github.com>
2025-01-28 16:22:59 -0300

    some more cleaning up

--- year/2015/04/solution.go
+++ year/2015/04/solution.go
@@ -13,21 +13,18 @@ var input string
 
 func main() {
 	input = strings.TrimSpace(input)
-	fmt.Println("part 1:", findHash(5))
-	fmt.Println("part 2:", findHash(6))
+	fmt.Println("sync part 1:", findHash(5))
+	fmt.Println("sync part 2:", findHash(6))
 }
 
 func findHash(zeroCount int) int {
 	zeroes := strings.Repeat("0", zeroCount)
-	i := 0
-	for {
-		i++
+	for i := 0; ; i++ {
 		key := input + fmt.Sprint(i)
 		hashBytes := md5.Sum([]byte(key))
 		hashStr := hex.EncodeToString(hashBytes[:])
 		if hashStr[0:zeroCount] == zeroes {
-			break
+			return i
 		}
 	}
-	return i
 }
