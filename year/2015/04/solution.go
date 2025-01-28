011bc6a5af5aa2dc5946c2e9604264d8a3f0072a
gkotecki <gkotecki@users.noreply.github.com>
2025-01-28 17:18:10 -0300

    add some parallelism - cpu go brr

--- year/2015/04/solution.go
+++ year/2015/04/solution.go
@@ -5,6 +5,7 @@ import (
 	_ "embed"
 	"encoding/hex"
 	"fmt"
+	"runtime"
 	"strings"
 )
 
@@ -13,6 +14,9 @@ var input string
 
 func main() {
 	input = strings.TrimSpace(input)
+	fmt.Println("parallel part 1:", findHashParallel(5))
+	fmt.Println("parallel part 2:", findHashParallel(6))
+	fmt.Println("parallel part X:", findHashParallel(7))
 	fmt.Println("sync part 1:", findHash(5))
 	fmt.Println("sync part 2:", findHash(6))
 }
@@ -27,4 +31,35 @@ func findHash(zeroCount int) int {
 			return i
 		}
 	}
+}
+
+func findHashParallel(zeroCount int) int {
+	zeroes := strings.Repeat("0", zeroCount)
+
+	numCPU := runtime.NumCPU()
+	result := make(chan int, numCPU)
+
+	for workerId := 0; workerId < numCPU; workerId++ {
+		go func(id int) {
+			for i := id; ; i += numCPU {
+				key := input + fmt.Sprint(i)
+				hashBytes := md5.Sum([]byte(key))
+				hashStr := hex.EncodeToString(hashBytes[:])
+				if hashStr[0:zeroCount] == zeroes {
+					result <- i
+					close(result)
+					return
+				}
+
+				select {
+				case <-result:
+					return
+				default:
+				}
+			}
+		}(workerId)
+	}
+
+	answer := <-result
+	return answer
 }
