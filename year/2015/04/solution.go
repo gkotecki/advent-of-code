32b0aeffbe1bd2e17c1b5b5a429fdc4cb81879a8
gkotecki <gkotecki@users.noreply.github.com>
2025-01-28 15:59:08 -0300

    cleaning up

--- year/2015/04/solution.go
+++ year/2015/04/solution.go
@@ -13,38 +13,21 @@ var input string
 
 func main() {
 	input = strings.TrimSpace(input)
-	part1()
-	part2()
+	fmt.Println("part 1:", findHash(5))
+	fmt.Println("part 2:", findHash(6))
 }
 
-func part1() {
+func findHash(zeroCount int) int {
+	zeroes := strings.Repeat("0", zeroCount)
 	i := 0
 	for {
 		i++
 		key := input + fmt.Sprint(i)
 		hashBytes := md5.Sum([]byte(key))
 		hashStr := hex.EncodeToString(hashBytes[:])
-		// fmt.Println(i, hashStr)
-		if hashStr[0:5] == "00000" {
+		if hashStr[0:zeroCount] == zeroes {
 			break
 		}
 	}
-	fmt.Println("part 1:", i)
-}
-
-func part2() {
-	i := 0
-	for {
-		i++
-		key := input + fmt.Sprint(i)
-		hashBytes := md5.Sum([]byte(key))
-		hashStr := hex.EncodeToString(hashBytes[:])
-		// if hashStr[0:5] == "00000" {
-		// 	fmt.Println(i, hashStr)
-		// }
-		if hashStr[0:6] == "000000" {
-			break
-		}
-	}
-	fmt.Println("part 2:", i)
+	return i
 }
